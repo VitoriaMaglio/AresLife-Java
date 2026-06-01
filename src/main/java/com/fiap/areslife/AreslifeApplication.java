10758624448c9ace09d9a23c538f92d9054abf15
VitoriaMaglio <vitoriamaglio@gmail.com>
2026-06-01 18:44:54 -0300

    Iniciando projeto GS.

---
+++ src/main/java/com/fiap/areslife/AreslifeApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.areslife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AreslifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AreslifeApplication.class, args);
+	}
+
+}
